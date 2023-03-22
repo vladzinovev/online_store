99324f670354c16d4ddb9fd51bcf20d25826f9d1
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-03-22 23:18:01 +0300

    connected Db and added diagram

--- server/db.js
+++ server/db.js
@@ -1,12 +1,18 @@
-import Sequelize from 'sequelize';
+import Sequelize from "sequelize";
+//const Sequelize = require('sequelize');
 
-module.exports= new Sequelize(
-    process.env.DB_NAME, //Название БД
-    process.env.DB_USER, //Пользователь
-    process.env.DB_PASSWORD, //Пароль
-    {
-        dialect:'postgres', //Тип БД
-        host: process.env.DB_HOST, //Хост
-        port: process.env.DB_PORT, //Порт
-    }
-)
+//для импорта env файла
+import dotenv from 'dotenv';
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME, //Название БД
+  process.env.DB_USER, //Пользователь
+  process.env.DB_PASSWORD, //Пароль
+  {
+    dialect: "postgres", //Тип БД
+    host: process.env.DB_HOST, //Хост
+    port: process.env.DB_PORT, //Порт
+  }
+);
+export default sequelize;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+
+//для импорта env файла
 import dotenv from 'dotenv';
-//require('dotenv')
+//require('dotenv') тоже самое что и сверху импорт
 dotenv.config();
+
+import sequelize from "./db.js";
+//const sequelize=require('./db.js');
+
 
 const port = process.env.PORT || 5000;
 
@@ -9,10 +15,24 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 
-//запускаем веб сервер на порте 4444
-app.listen(port, (err)=>{
-    if (err){
-        return console.log(err);
+//вызываем функцию для подключения к БД
+const start = async () => {
+    try {
+      //с помощью нее устанавливается подключение к БД
+      await sequelize.authenticate();
+      //сверяет состояние БД со схемой данных
+      await sequelize.sync();
+  
+      //запускаем веб сервер на порте 4444
+      app.listen(port, (err) => {
+        if (err) {
+          return console.log(err);
+        }
+        console.log(`Server OK ${process.env.port}`);
+      });
+  
+    } catch (e) {
+      console.log(e);
     }
-    console.log(`Server OK ${process.env.PORT}`);
-})
+  };
+  start();
