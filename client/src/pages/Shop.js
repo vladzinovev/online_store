ae80dc3db4cb7e3fab237666f4a1068251d0c8bb
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-03-28 23:49:26 +0300

    created BrandBar

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -3,6 +3,7 @@ import {Container} from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import TypeBar from "../components/TypeBar";
+import BrandBar from "../components/BrandBar";
 
 const Shop = () => {
     
@@ -14,6 +15,7 @@ const Shop = () => {
                     <TypeBar/>
                 </Col>
                 <Col md={9}>
+                    <BrandBar/>
                 </Col>
             </Row>
         </Container>
