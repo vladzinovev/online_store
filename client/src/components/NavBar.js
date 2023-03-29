a5491b2b05ce2cbad3af1821197adb48a9fc2b10
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-03-29 23:24:32 +0300

    edited DevicePage

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,13 +3,19 @@ import {Context} from "../index";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import {NavLink} from "react-router-dom";
-import {SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {Button} from "react-bootstrap";
 import {observer} from "mobx-react-lite";
 import Container from "react-bootstrap/Container";
-
+import {useHistory} from 'react-router-dom'
 const NavBar = observer(() => {
     const {user} = useContext(Context)
+    const history = useHistory()
+
+    const logOut = () => {
+        user.setUser({})
+        user.setIsAuth(false)
+    }
 
     return (
         <Navbar bg="dark" variant="dark">
@@ -19,11 +25,13 @@ const NavBar = observer(() => {
                     <Nav className="ml-auto" style={{color: 'white'}}>
                         <Button
                             variant={"outline-light"}
+                            onClick={() => history.push(ADMIN_ROUTE)}
                         >
                             Админ панель
                         </Button>
                         <Button
                             variant={"outline-light"}
+                            onClick={() => logOut()}
                             className="ml-2"
                         >
                             Выйти
@@ -31,12 +39,11 @@ const NavBar = observer(() => {
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{color: 'white'}}>
-                        <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+                        <Button variant={"outline-light"} onClick={() => history.push(LOGIN_ROUTE)}>Авторизация</Button>
                     </Nav>
                 }
             </Container>
         </Navbar>
-
     );
 });
 
