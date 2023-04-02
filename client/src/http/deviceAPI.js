af6068c35c1949ac721bd7bd4dd0bf9d40c38610
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-04-02 15:52:26 +0300

    created deviceAPI

---
+++ client/src/http/deviceAPI.js
@@ -0,0 +1,47 @@
+import {$authHost, $host} from "./index";
+import jwt_decode from "jwt-decode";
+
+//создание типа
+export const createType = async (type) => {
+    //только ($authHost) авторизованный пользватель "admin"
+    const {data} = await $authHost.post('api/type', type)
+    return data
+}
+
+//получение типа
+export const fetchTypes = async () => {
+    const {data} = await $host.get('api/type')
+    return data
+}
+
+//создание бренда
+export const createBrand = async (brand) => {
+    const {data} = await $authHost.post('api/brand', brand)
+    return data
+}
+
+//создание бренда
+export const fetchBrands = async () => {
+    const {data} = await $host.get('api/brand', )
+    return data
+}
+
+//создание девайса
+export const createDevice = async (device) => {
+    const {data} = await $authHost.post('api/device', device)
+    return data
+}
+
+//создание девайсов
+export const fetchDevices = async (typeId, brandId, page, limit= 5) => {
+    const {data} = await $host.get('api/device', {params: {
+            typeId, brandId, page, limit
+        }})
+    return data
+}
+
+//создание девайса
+export const fetchOneDevice = async (id) => {
+    const {data} = await $host.get('api/device/' + id)
+    return data
+}
